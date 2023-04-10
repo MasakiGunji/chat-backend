367d693e18ee939d81e17bbcc09d9a02acaa691f
masaki <gmd.1126@icloud.com>
2023-04-10 15:08:24 +0900

    first commit

---
+++ src/main/java/com/lavulite/techpit/chat/chatbackend/domain/channels/service/ChannelDomainService.java
@@ -0,0 +1,40 @@
+package com.lavulite.techpit.chat.chatbackend.domain.channels.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.lavulite.techpit.chat.chatbackend.domain.channels.model.Channel;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ChannelDomainService {
+
+  private final ChannelRepository channelRepository;
+  
+  public Channel create(Channel channel){
+    var currentMaxId = channelRepository.getMaxId();
+
+    var newid = currentMaxId.orElse(0) + 1;
+    channel.setId(newid);
+
+    channelRepository.insert(channel);
+
+    return channel;
+  }  
+
+  public List<Channel> findAll(){
+    return channelRepository.findAll();
+  }
+
+  public Channel update(Channel channle){
+    channelRepository.update(channle);
+    return channle;
+  }
+
+  public void delete(int id){
+    channelRepository.delete(id);
+  }
+}
---
+++ src/main/java/com/lavulite/techpit/chat/chatbackend/domain/messages/service/MessageDomainService.java
@@ -0,0 +1,34 @@
+package com.lavulite.techpit.chat.chatbackend.domain.messages.service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.lavulite.techpit.chat.chatbackend.domain.messages.model.Message;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MessageDomainService {
+  
+  private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyyMMddhhmm");
+  private final MessageRepository messageRepository;
+
+  public Message post(Message message) {
+    var uuid = UUID.randomUUID();
+    var now = LocalDateTime.now();
+    message.setId(now.format(FORMATTER) + "-" + uuid.toString());
+    message.setTimestamp(now);
+    messageRepository.insert(message);
+    return message;
+  } 
+
+  public List<Message> find(int channelId, Optional<String> searchWord) {
+    return messageRepository.find(channelId, searchWord);
+  }
+}
